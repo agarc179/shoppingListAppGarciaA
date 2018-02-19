acd08a7c7d4b2c104cb19c7702dd2bcfa0db8008
AdrianG <tej18359@gmail.com>
2018-02-18 20:53:37 -0600

    finished app
    bug free app it now checks if the quantity field has an int else will alert user, comments are also put in

--- shoppingListAppGarciaA/ViewController.swift
+++ shoppingListAppGarciaA/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //boolean funtion to check if a String is an Int
+    func isInt(string: String) -> Bool{
+        return Int(string) != nil
+    }
 
     @IBOutlet weak var listLabel: UILabel!
     
@@ -26,63 +31,68 @@ class ViewController: UIViewController {
     }
 
     @IBAction func newList(_ sender: UIButton) {
+        // reset the list to No Item
         listLabel.text = "No Item"
     }
     
     @IBAction func newItem(_ sender: UIButton) {
+        // reset the text fields to empty
         for tf in textFields{
             tf.text = ""
         }
     }
     
     @IBAction func addItem(_ sender: UIButton) {
-        if listLabel.text == "No Item"{
-            listLabel.text = ""
-        }
         var items : [Int:String] = [:]
         for tf in textFields{
             tf.resignFirstResponder()
             
-            if tf.text != ""{
+            // if text fields are not empty AND quantity field is an Int add to the dictionary
+            if tf.text != "" && isInt(string: textFields[1].text!){
                 if let tfText = tf.text{
                     items[tf.tag] = tfText
                 }
             }
-            
-            if tf.text == ""{
+    
+            // if text fields are empty OR quantity field is not an Int send alert
+            else if tf.text == "" || isInt(string: textFields[1].text!) == false{
                 let alertController = UIAlertController(title: "Error", message: "Invalid Item", preferredStyle: .alert)
                 let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
                 alertController.addAction(cancelAction)
                 present(alertController, animated: true, completion: nil)
             }
         }
-        // if dictionary is not empty add to the listLabel
-        if items.count != 0{
+        
+        if listLabel.text == "No Item" && items.count == 2{
+            listLabel.text = ""
+        }
+        
+        // if dictionary has 2 items it will add to the listLabel
+        if items.count == 2{
+            // adds the quantity of items
             if let quantity = items[2]{
-                print(quantity)
                 listLabel.text = (listLabel.text ?? "") + "\(quantity)x "
             }
             
+            // adds the description of the item
             if let description = items[1]{
                 listLabel.text = (listLabel.text ?? "") + "\(description)"
             }
             
-            listLabel.text = (listLabel.text ?? "") + "\n"
+            listLabel.text = (listLabel.text ?? "") + "\n" // new line
         }
     }
     
+    // when the action button is pressed, the soft keyboard is dismissed
     @IBAction func editEnded(_ sender: UITextField) {
         sender.resignFirstResponder()
     }
     
+    // when background is touch, the soft keyboard is dismissed
     @IBAction func backgroundTouched(_ sender: UIControl) {
         for tf in textFields{
             tf.resignFirstResponder()
         }
     }
-    
-    
-    
-    
 }
 
